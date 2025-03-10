b6a9623a788e12d41ae59f18935adddd2108a778
Glitch (defiant-stone-tail) <none>
2025-03-10 11:37:33 +0000

    🤗🌹 Checkpoint ./config/redis.js:65144299/798

--- config/redis.js
+++ config/redis.js
@@ -1,10 +1,12 @@
 const redis = require('redis');
 
 const client = redis.createClient({
-  url: process.env.REDIS_URL || 'redis://localhost:6379',
+  url: process.env.REDIS_URL || 'rediss://default:AezpAAIjcDEwOWU2NWVlODdhZDA0OGM4ODdkNmYxODM2MWM1ODFjNnAxMA@dominant-walrus-60649.upstash.io:6379',
 });
 
 client.on('error', (err) => console.error('Redis error:', err));
 client.connect();
 
 module.exports = client;
+
+
